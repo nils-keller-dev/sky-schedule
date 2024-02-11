80b84d5dcaec18b73a2b84f39afc92b6dbc74116
tsomic <keller.nils99@gmail.com>
2024-02-12 00:52:07 +0100

    ✨ add flightradarapi

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,37 @@
 import fastify from 'fastify'
+import { FlightRadar24API } from 'flightradarapi'
 
+const frApi = new FlightRadar24API()
 const app = fastify({ logger: true })
 
-app.get('/', async (_request, _reply) => {
-  return { hello: 'world' }
-})
+const routeOptions = {
+  schema: {
+    querystring: {
+      type: 'object',
+      properties: {
+        lat: { type: 'number' },
+        long: { type: 'number' },
+        radius: { type: 'number' },
+      },
+      required: ['lat', 'long', 'radius'],
+    },
+  },
+}
+
+interface BoundsQuery {
+  lat: number
+  long: number
+  radius: number
+}
+
+app.get<{ Querystring: BoundsQuery }>(
+  '/',
+  routeOptions,
+  async ({ query: { lat, long, radius } }) => {
+    const bounds = frApi.getBoundsByPoint(lat, long, radius)
+    const flights = await frApi.getFlights(null, bounds)
+    return { flights }
+  }
+)
 
 export default app
