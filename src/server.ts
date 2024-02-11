ab3659a4eef50a94a79e0f210c79a84d93a59d86
tsomic <keller.nils99@gmail.com>
2024-02-11 23:38:05 +0100

    ✨ use port from .env

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import app from './app'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const start = async () => {
   try {
-    await app.listen({ port: 3000 })
-    console.log('Server running at http://localhost:3000/')
+    const port = parseInt(process.env.PORT || '3000')
+
+    await app.listen({ port })
+    console.log(`Server running at http://localhost:${port}/`)
   } catch (err) {
     app.log.error(err)
     process.exit(1)
