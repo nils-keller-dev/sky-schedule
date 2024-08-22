f42f0377bb2309bb6601726a63b961e0d3143b43
tsomic <keller.nils99@gmail.com>
2024-08-22 23:44:24 +0200

    🐛 set 0.0.0.0 as server host

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,9 @@ dotenv.config()
 const start = async () => {
   try {
     const port = parseInt(process.env.PORT || '3000')
+    const host = '0.0.0.0'
 
-    await app.listen({ port })
+    await app.listen({ port, host })
     console.log(`Server running at http://localhost:${port}/`)
   } catch (err) {
     app.log.error(err)
