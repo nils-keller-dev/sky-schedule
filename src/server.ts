94370fb603e8f03d0f71703e3d7eae23b7469ac6
tsomic <keller.nils99@gmail.com>
2024-12-12 15:55:06 +0100

    ✨ add user specific default configs

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,6 @@ import closestPlane from './app.ts'
 import { verifyClient } from './utils/authentication.ts'
 import { buildLogArray, log } from './utils/utils.ts'
 
-const routes = {
-  'closestPlane': closestPlane,
-}
-
 const port = parseInt(Deno.env.get('PORT') ?? '3000')
 const hostname = '0.0.0.0'
 
@@ -23,15 +19,28 @@ Deno.serve({ port, hostname }, async (request: Request) => {
   console.log(request.url)
 
   const fullPath = request.url.split('/')[3].split('?')
-  const searchParams = new URLSearchParams(fullPath[1])
-  const queryObject = Object.fromEntries(searchParams.entries())
 
-  const routeFunction = routes[fullPath[0] as keyof typeof routes]
-  if (!routeFunction) {
+  if (fullPath[0] !== 'closestPlane') {
     return new Response('Not found', { status: 404 })
   }
 
-  const response = await routeFunction?.(queryObject)
+  const searchParams = new URLSearchParams(fullPath[1])
+  const queryObject = Object.fromEntries(searchParams.entries())
+
+  let userConfig = {}
+
+  try {
+    userConfig = (await import(`./configs/${name}.json`, {
+      with: { type: 'json' },
+    })).default
+  } catch (_) {
+    console.log('User config not found')
+  }
+
+  const response = await closestPlane({
+    ...userConfig,
+    ...queryObject,
+  })
 
   if (!response) {
     return new Response('Bad request', { status: 400 })
